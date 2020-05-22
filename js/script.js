534092de6dd21e8dfc465b130676cbbc7819ab35
garciamendes <garciaprog0101@gmail.com>
2020-05-22 20:54:41 -0300

    creating a small calculator

---
+++ js/script.js
@@ -0,0 +1,27 @@
+feather.replace()
+
+let date = new Date().getFullYear()
+
+document.getElementById('data').innerHTML = date
+
+function insert(num) {
+  document.form.textview.value = document.form.textview.value + num
+}
+
+function equal() {
+  let exp = document.form.textview.value
+
+  if (exp) {
+    document.form.textview.value = eval(exp)
+  }
+}
+
+function clean() {
+  document.form.textview.value = ""
+}
+
+function remove() {
+  let exp = document.form.textview.value
+
+  document.form.textview.value = exp.substring(0, exp.length - 1)
+}
